44e9d80d0f1f99dca3b0e0c5192896ac417b3b71
mdranavai0 <mdranavai0145@gmail.com>
2025-09-09 07:47:24 +0400

    Update Info.js

--- Script/commands/Info.js
+++ Script/commands/Info.js
@@ -65,10 +65,10 @@ module.exports.run = async function ({ api, event, args, Users, Threads }) {
  😍─꯭─⃝‌‌𝐑𝐀𝐍𝐀 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭😘`;
 
  const imgLinks = [
- "https://i.imgur.com/zqsuJnX.jpeg",
- "https://i.imgur.com/sxSn1K3.jpeg",
- "https://i.imgur.com/wu0iDqS.jpeg",
- "https://i.imgur.com/Huz3nAE.png"
+ "https://i.imgur.com/ENOZJM9.jpeg",
+ "https://i.imgur.com/ENOZJM9.jpeg",
+ "https://i.imgur.com/ENOZJM9.jpeg",
+ "https://i.imgur.com/ENOZJM9.jpeg"
  ];
 
  const imgLink = imgLinks[Math.floor(Math.random() * imgLinks.length)];
